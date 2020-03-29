a1b0245012d389b515daa8ce07856052be83b65e
Vova <vova@Vovas-Mac.local>
2020-03-29 13:05:09 +0300

    29.03 - Vova_2

--- server/inc/uchat.h
+++ server/inc/uchat.h
@@ -27,10 +27,6 @@
 #define MX_PATH_TO_DB "./server_db"
 #define REPORT_FILENAME "/tmp/uchat_log.txt"
 
-#define BUFLEN 128
-#define QLEN 10
-#define HOST_NAME_MAX 256
-
 typedef struct  s_clients {
     pthread_t tid; // thread id
     int fd; // transporting fd
@@ -45,46 +41,46 @@ typedef struct  s_info {
     int argc;
     char **argv;
 
-    char *ip;
+    char *ip; // my ip address
     uint16_t port;
-    struct s_clients *clients;
+    struct s_clients *clients; // структура де зберігаються усі клієнти, що приєдналися
 
-    sqlite3 *db;
+    sqlite3 *db; // our database
 
     pthread_mutex_t mutex;
 }               t_info;
 
 // Folder: help_funcs
-void mx_err_exit(char *err_msg);
+void mx_err_exit(char *err_msg); // вивести помилку і тоді вийти з програми
 
 void mx_sigio_handler(int sigio);
-int mx_init_info(t_info **info, int argc, char **argv);
-void mx_daemonize(t_info *info);
-void mx_start_server(t_info *info);
+int mx_init_info(t_info **info, int argc, char **argv); // initialization of main structure (info)
+void mx_daemonize(t_info *info); // перейти в режим демона
+void mx_start_server(t_info *info); // створення сокетів та потоків для клієнтів
 
 // client_step ################################################################
-void *mx_start_client(void *argument);
+void *mx_start_client(void *argument); // коли клієнт запускає програму, то попадає сюди
 
 // work_with_clients.c
-char *mx_read_client_input(int fd);
+char *mx_read_client_input(int fd); // функція для зчитування повідомлення від клієнта
 
 // check_client_requests.c
-t_clients *mx_user_online(t_info *info, char *login);
+t_clients *mx_user_online(t_info *info, char *login); // перевірити в структурі чи є клієнт з таким логіном
 
-void mx_clear_form(t_clients *client_info);
+void mx_clear_form(t_clients *client_info); // затирати інформацію, під час введення даних для sql
 
 // Folder: client_authorization
-void mx_chat_connect(t_info *info, t_clients *client_info);
-void mx_sign_in(t_info *info, t_clients *client_info);
-void mx_sign_up(t_info *info, t_clients *client_info);
+void mx_chat_connect(t_info *info, t_clients *client_info); // меню авторизації (sign in, sign up)
+void mx_sign_in(t_info *info, t_clients *client_info); // залогінитися (якщо все вірно, одразу переходить в меню чата)
+void mx_sign_up(t_info *info, t_clients *client_info); // зареєструватися
 
 // Folder: client_in_chat.c
-void mx_chat_menu(t_info *info, t_clients *client_info);
-void mx_message_received(char *input, t_info *info, t_clients *client_info);
+void mx_chat_menu(t_info *info, t_clients *client_info); // після авторизації, клієнт попадає у це меню
+void mx_message_received(char *input, t_info *info, t_clients *client_info); // обробка повідомлення клієнта
 
-
-t_clients *mx_create_new_client(int fd, pthread_t tid);
-void mx_push_client_back(t_clients **list, int fd, pthread_t tid);
-void mx_pop_client_front(t_clients **head);
+// work_with_clients_list.c
+t_clients *mx_create_new_client(int fd, pthread_t tid); // створити новий елемент ліста клієнти
+void mx_push_client_back(t_clients **list, int fd, pthread_t tid); // добавити новий елемент ліста на початок структури клієнти
+void mx_pop_client_front(t_clients **head); // видалити передній елемент ліста клієнти
 
 #endif
