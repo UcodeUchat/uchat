4c4d38d8cf642a3ca22906df4cb452c2ed219e67
MSM <nikolayenkoserhiy@gmail.com>
2020-04-01 12:52:18 +0300

    1

--- src/client/main_client.c
+++
@@ -1,9 +0,0 @@
-#include "inc/uchat.h"
-
-int main(int argc, char **argv) {
-    t_info_client *info = NULL;
-
-    mx_init_info_client(&info, argc, argv);
-    mx_start_client2(info);
-    return 0;
-}
