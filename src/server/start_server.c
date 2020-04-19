49650557236882433a0e8de219ac8563e9c0211c
MSM <nikolayenkoserhiy@gmail.com>
2020-04-19 12:51:16 +0300

    add TLS

--- src/server/start_server.c
+++ src/server/start_server.c
@@ -1,13 +1,24 @@
 #include "uchat.h"
 
+//#define CERT_FILE "../certs/server-cert.pem"
+//#define KEY_FILE  "../certs/server-key.pem"
+
+
 int mx_start_server(t_server_info *info) {
     int server;
     struct addrinfo hints;
     struct addrinfo *bind_address;
     struct sockaddr_in serv_addr;
+    SSL_CTX *ctx;
+
+
+    SSL_library_init();
+    ctx = mx_init_server_ctx();  // initialize SSL
+    mx_load_certificates(ctx,  "mycert.pem", "mycert.pem"); // load cert
+    printf("2--------++++\n");
+
 
     printf("Configuring local address...\n");
-
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
@@ -82,6 +93,28 @@ int mx_start_server(t_server_info *info) {
                 printf("error = %s\n", strerror(errno));
                 break;
             }
+
+            // TLS
+            SSL *ssl = SSL_new(ctx);
+            if (!ctx) {
+                fprintf(stderr, "SSL_new() failed.\n");
+                return 1;
+            }
+            printf("SSL/TLS using %s\n", SSL_get_cipher(ssl));
+//        mx_show_certs(ssl);
+            printf("6--------++++\n");
+            SSL_set_fd(ssl, client_sock);
+
+            /* Establish TLS connection */
+            ret = wolfSSL_accept(ssl);
+            if (ret != SSL_SUCCESS) {
+                fprintf(stderr, "wolfSSL_accept error = %d\n",
+                        wolfSSL_get_error(ssl, ret));
+                return -1;
+            }
+
+            printf("Client connected successfully\n");
+
 //            int old_flags  = fcntl(client_sock, F_GETFL, 0);
 //            fcntl(client_sock, F_SETFL, old_flags | O_NONBLOCK);
         }
@@ -91,10 +124,13 @@ int mx_start_server(t_server_info *info) {
             if ((new_ev.flags & EV_EOF) != 0) {
                 printf("Client %lu disconnected\n", new_ev.ident);
                 mx_drop_socket(info, new_ev.ident);
+
                 close(new_ev.ident);
             }
             else {
-                int rc = mx_worker(new_ev.ident, info);
+
+                int rc = mx_worker_ssl(ssl, new_ev.ident, info);
+//                int rc = mx_worker(new_ev.ident, info);
                 if (rc == -1) {
                     printf("error = %s\n", strerror(errno));
                     break;
@@ -104,4 +140,38 @@ int mx_start_server(t_server_info *info) {
     }
     return 0;
 }
+
+SSL_CTX* create_context(void) {
+    SSL_CTX *ctx;
+
+    OpenSSL_add_all_algorithms();  // load & register all cryptos, etc.
+    SSL_load_error_strings();  // load all error messages
+    ctx = SSL_CTX_new(TLS_server_method());  // create new context from method
+    if (ctx == NULL ) {
+        ERR_print_errors_fp(stderr);
+        abort();
+    }
+    return ctx;
+}
+
+void mx_load_certificates(SSL_CTX* ctx, char* cert_file, char* key_file) {
+    // set the local certificate from CertFile
+
+    if (SSL_CTX_use_certificate_file(ctx, cert_file, SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+        abort();
+    }
+    // set the private key from KeyFile (may be the same as CertFile)
+    if (SSL_CTX_use_PrivateKey_file(ctx, key_file, SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+        abort();
+    }
+    // verify private key
+    if (!SSL_CTX_check_private_key(ctx)) {
+        fprintf(stderr, "Private key does not match the public certificate\n");
+        abort();
+    }
+    printf("load_cer sucsess\n");
+}
+
 
