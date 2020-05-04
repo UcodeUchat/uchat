3c1907fd63c5ac42dd445776c30a7b0fb541185f
Admin <admin@Mac-Admin.local>
2020-05-04 12:53:50 +0300

    megre

--- src/client/input_from_server.c
+++ src/client/input_from_server.c
@@ -48,11 +48,11 @@ void *mx_process_input_from_server(void *taken_info) {
             if (input_package->type == MX_FILE_SEND_TYPE)
                 mx_process_file_in_client(info, input_package);
             //mx_print_curr_time();
-            if (input_package->add_info == MX_AUTH_TYPE || input_package->add_info == MX_AUTH_TYPE_V ||
-                input_package->add_info == MX_AUTH_TYPE_NV) {
+            if (input_package->type == MX_AUTH_TYPE || input_package->type == MX_AUTH_TYPE_V ||
+                input_package->type == MX_AUTH_TYPE_NV) {
                 if ((*info).auth_client == 0){
-                    fprintf(stderr, "ANSWER = [%d]\n", input_package->add_info);
-                    if (input_package->add_info == 4) {
+                    fprintf(stderr, "ANSWER = [%d]\n", input_package->type);
+                    if (input_package->type == 4) {
                         info->id = input_package->user_id;
                         (*info).auth_client = 1;
                     }
@@ -62,10 +62,10 @@ void *mx_process_input_from_server(void *taken_info) {
                     (*info).responce = 1;
                 }
             }
-            else if (input_package->add_info == MX_REG_TYPE_V || input_package->add_info == MX_REG_TYPE_NV) {
+            else if (input_package->type == MX_REG_TYPE_V || input_package->type == MX_REG_TYPE_NV) {
 
             }
-            else if (input_package->add_info == MX_MSG_TYPE) {
+            else if (input_package->type == MX_MSG_TYPE) {
                 t_room *room = mx_find_room(info->data->rooms, input_package->room_id);
                 GtkWidget *h_box = gtk_box_new(FALSE, 0);
                 gtk_box_pack_start (GTK_BOX (room->message_box), h_box, FALSE, FALSE, 0);
