318439dd78f94989c61142175a22b8b35f7dd193
Vova <vova@Vovas-Mac.local>
2020-05-10 12:00:35 +0300

    little changes

--- src/client/send_file_from_client.c
+++ src/client/send_file_from_client.c
@@ -4,33 +4,49 @@ bool pick_file_to_send(t_client_info *info, FILE **file, json_object **obj) {
     (void)info;
     printf("pick_file_to_send\n");
 
+    struct stat file_stat;
+// tmp
     char file_path[256] = {0};
     char file_name[256] = {0};
     scanf("%s", file_path);
     strcat(file_name, file_path);
-    //
-    if ((*file = fopen(file_path, "r")) != NULL) {
-        struct stat file_stat;
-
-        stat(file_path, &file_stat);
-        if (file_stat.st_size > 0 && file_stat.st_size <= MX_MAX_FILE_SIZE) {
-        // need to lock file editing
-            *obj = mx_create_basic_json_object(MX_FILE_SEND_TYPE);
-            json_object_object_add(*obj, "file_name", json_object_new_string(file_name));
-            json_object_object_add(*obj, "file_size", json_object_new_int(file_stat.st_size));
-            return 0;
-        //
+    if (stat(file_path, &file_stat) != MX_OK)
+        return 1;
+//
+    if (file_stat.st_size > 0 && file_stat.st_size <= MX_MAX_FILE_SIZE) {
+        if (!(file_stat.st_mode & S_IFDIR) && (file_stat.st_mode & S_IFREG)) {
+            if ((*file = fopen(file_path, "r")) != NULL) {
+                *obj = mx_create_basic_json_object(MX_FILE_SEND_TYPE);
+                json_object_object_add(*obj, "file_name", json_object_new_string(file_name));
+                json_object_object_add(*obj, "file_size", json_object_new_int(file_stat.st_size));
+                return 0;
+            }
+            else
+                fprintf(stderr, "Хм, під час відкривання файлу щось пішло не так\n");
         }
+        else
+            fprintf(stderr, "Йой, непідтримуваний тип файлу\n");
     }
+    else
+        fprintf(stderr, "Схоже, що твій файл надто великий\nМаксимальний розмір файлу %d\n", MX_MAX_FILE_SIZE);
     return 1;
 }
-
+// struct stat file_stat;
+// stat(file_path, &file_stat);
+// printf("S_IFMT:%d, S_IFSOCK:%d, S_IFLNK:%d, S_IFREG:%d, S_IFBLK:%d, S_IFDIR:%d, S_IFCHR:%d, S_IFIFO:%d\n",
+// file_stat.st_mode & S_IFMT ? 1 : 0,
+// file_stat.st_mode & S_IFSOCK ? 1 : 0,
+// file_stat.st_mode & S_IFLNK ? 1 : 0,
+// file_stat.st_mode & S_IFREG ? 1 : 0,
+// file_stat.st_mode & S_IFBLK ? 1 : 0,
+// file_stat.st_mode & S_IFDIR ? 1 : 0,
+// file_stat.st_mode & S_IFCHR ? 1 : 0,
+// file_stat.st_mode & S_IFIFO ? 1 : 0);
 void mx_send_file_from_client(t_client_info *info) {
     FILE *file;
     json_object *send_obj;
 
     if (pick_file_to_send(info, &file, &send_obj) == 0) {
-        printf("file picked\n");
         int readed = 1;
         const char *json_string;
         char buffer[1024];
