3488c9e90c8298ef83b70160c7a83865c9fec986
NHokage <maksimlibovich@ukr.net>
2020-06-05 15:11:06 +0300

    delete acc button auditor

--- src/client/login.c
+++ src/client/login.c
@@ -634,6 +634,39 @@ void init_menu_button (t_client_info *info, GtkWidget *box, char *text,
     gtk_widget_show(box1);
 }
 
+GtkWidget *init_delete_menu (t_client_info *info) {
+    GtkWidget *answer_menu = gtk_menu_new();
+    GtkWidget *item_question = gtk_menu_item_new_with_label("Delete or not?");
+    GtkWidget *item_no = gtk_menu_item_new_with_label("Nope");
+    GtkWidget *item_yep = gtk_menu_item_new_with_label("Yep");
+
+    gtk_widget_set_sensitive (item_question, FALSE);
+    gtk_widget_show(item_question);
+    gtk_menu_shell_append (GTK_MENU_SHELL (answer_menu), item_question);
+    gtk_widget_show(item_no);
+    gtk_menu_shell_append (GTK_MENU_SHELL (answer_menu), item_no);
+    gtk_widget_show(item_yep);
+    gtk_menu_shell_append (GTK_MENU_SHELL (answer_menu), item_yep);
+    g_signal_connect (G_OBJECT (item_yep), "activate", G_CALLBACK (yep_callback), info);
+    return answer_menu;
+}
+
+void init_delete_button (t_client_info *info, GtkWidget *box, char *text,
+                        void (*callback) (GtkWidget *widget, GtkWidget *answer_menu)) {
+    GtkWidget *box1 = gtk_box_new(FALSE, 0);
+    GtkWidget *button = gtk_button_new_with_label(text);
+    GtkWidget *answer_menu = init_delete_menu(info);
+
+    gtk_widget_set_halign(box1, GTK_ALIGN_CENTER);
+    gtk_box_pack_start(GTK_BOX (box), box1, TRUE, FALSE, 0);
+    g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(callback), answer_menu);
+    gtk_box_pack_start(GTK_BOX (box1), button, TRUE, FALSE, 0);
+    gtk_widget_set_size_request(button, 100, -1);
+    gtk_widget_set_name(button, "entry");
+    gtk_widget_show(button);
+    gtk_widget_show(box1);
+}
+
 void init_menu (t_client_info *info) {
     GtkWidget *main_box = NULL;
     GtkWidget *fixed = NULL;
@@ -650,33 +683,8 @@ void init_menu (t_client_info *info) {
     init_menu_button (info, box, "Create room", create_callback);
     init_menu_button (info, box, "Profile", profile_callback);
     init_menu_button (info, box, "Logout", logout_callback);
+    init_delete_button (info, box, "Delete acc", delete_acc_callback);
     exit_box = init_menu_exit_box(info, info->data->menu, close_menu_callback);
-
-
-    //create delete_menu yes/no
-    GtkWidget *answer_menu = gtk_menu_new();
-    GtkWidget *item_question = gtk_menu_item_new_with_label("Delete or not?");
-    gtk_widget_set_sensitive (item_question, FALSE);
-    gtk_widget_show(item_question);
-    gtk_menu_shell_append (GTK_MENU_SHELL (answer_menu), item_question);
-    GtkWidget *item_no = gtk_menu_item_new_with_label("Nope");
-    gtk_widget_show(item_no);
-    gtk_menu_shell_append (GTK_MENU_SHELL (answer_menu), item_no);
-    GtkWidget *item_yep = gtk_menu_item_new_with_label("Yep");
-    gtk_widget_show(item_yep);
-    gtk_menu_shell_append (GTK_MENU_SHELL (answer_menu), item_yep);
-    g_signal_connect (G_OBJECT (item_yep), "activate", G_CALLBACK (yep_callback), info);
-
-    GtkWidget *box1 = gtk_box_new(FALSE, 0);
-    gtk_widget_set_halign (box1, GTK_ALIGN_CENTER);
-    gtk_box_pack_start (GTK_BOX (box), box1, TRUE, FALSE, 0);
-    GtkWidget *button = gtk_button_new_with_label("Delete acc");
-    g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(delete_acc_callback), answer_menu);
-    gtk_box_pack_start (GTK_BOX (box1), button, TRUE, FALSE, 0);
-    gtk_widget_set_size_request(button, 100, -1);
-    gtk_widget_set_name(button, "entry");
-    gtk_widget_show(button);
-    gtk_widget_show(box1);
 }
 
 void item_callback (GtkWidget *widget, t_stik *stik) {
