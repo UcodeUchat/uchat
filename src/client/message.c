e5b773bf4d88215d8b49a79605a6db4f92acce5a
NHokage <maksimlibovich@ukr.net>
2020-06-05 18:23:37 +0300

    message.c auditor 01

--- src/client/message.c
+++ src/client/message.c
@@ -137,79 +137,96 @@ void choose_side (t_client_info *info, t_message *node, t_room *room, int order)
     }
 }
 
+void simple_message (t_message *node, const char *message) {
+    node->message_box =  gtk_box_new(FALSE, 0);
+    gtk_widget_show(node->message_box);
+    gtk_box_pack_start (GTK_BOX (node->central_box), node->message_box, FALSE, FALSE, 0);
+    node->message_label = gtk_label_new(message);
+    gtk_widget_show(node->message_label);
+    gtk_box_pack_start (GTK_BOX (node->message_box), node->message_label, FALSE, FALSE, 0);
+}
+
+void file (t_mes *mes, t_message *node, const char *message) {
+    GtkWidget *label = gtk_label_new(message + 20);
+
+    node->message_box = gtk_event_box_new();
+    gtk_widget_show(node->message_box);
+    gtk_box_pack_start (GTK_BOX (node->central_box), node->message_box, FALSE, FALSE, 0);
+    gtk_widget_show(label);
+    gtk_container_add (GTK_CONTAINER (node->message_box), label);
+    gtk_widget_set_name(node->message_box, "file");
+    gtk_widget_add_events (node->message_box, GDK_BUTTON_PRESS_MASK);
+    g_signal_connect (G_OBJECT (node->message_box), "button_press_event", G_CALLBACK (file_callback), mes);
+    g_signal_connect (G_OBJECT (node->message_box), "button_release_event", G_CALLBACK (file1_callback), mes);
+    gtk_widget_add_events (node->message_box, GDK_ENTER_NOTIFY_MASK);
+    g_signal_connect (G_OBJECT (node->message_box), "enter_notify_event", G_CALLBACK (file_notify_callback), mes);
+    g_signal_connect (G_OBJECT (node->message_box), "leave_notify_event", G_CALLBACK (file_notify1_callback), mes);
+}
+
+void image (t_message *node) {
+    node->message_box = gtk_box_new(FALSE, 0);
+    gtk_widget_show(node->message_box);
+    gtk_box_pack_start (GTK_BOX (node->central_box), node->message_box, FALSE, FALSE, 0);
+    node->image_box = gtk_box_new(FALSE, 0);
+    gtk_widget_show(node->image_box);
+    gtk_box_pack_start (GTK_BOX (node->message_box), node->image_box, FALSE, FALSE, 0);
+}
+
+void sticker (t_message *node, const char *message) {
+    GtkWidget *stick_box =  gtk_box_new(FALSE, 0);
+    GdkPixbuf *item_pixbuf = gdk_pixbuf_new_from_file_at_scale (message, 200, 200, TRUE, NULL);
+    GtkWidget *item_image = gtk_image_new_from_pixbuf(item_pixbuf);
+
+    gtk_widget_show(stick_box);
+    gtk_box_pack_start (GTK_BOX (node->central_box), stick_box, FALSE, FALSE, 0);
+    gtk_widget_show(item_image);
+    gtk_box_pack_start (GTK_BOX (stick_box), item_image, FALSE, FALSE, 0);
+}
+
+void audio (t_mes *mes, t_message *node, const char *message) {
+    node->message_box = gtk_event_box_new();
+    gtk_widget_show(node->message_box);
+    gtk_box_pack_start (GTK_BOX (node->central_box), node->message_box, FALSE, FALSE, 0);
+    GtkWidget *label2 = gtk_label_new(message + 20);
+    gtk_widget_show(label2);
+    gtk_container_add (GTK_CONTAINER (node->message_box), label2);
+    gtk_widget_set_name(node->message_box, "file");
+    gtk_widget_add_events (node->message_box, GDK_BUTTON_PRESS_MASK);
+    g_signal_connect (G_OBJECT (node->message_box), "button_press_event", G_CALLBACK (load_audio_callback), mes);
+}
+
 void choose_type (t_mes *mes, t_message *node, const char *message) {
-    if (node->add_info == 0) {
-        node->message_box =  gtk_box_new(FALSE, 0);
-        gtk_widget_show(node->message_box);
-        gtk_box_pack_start (GTK_BOX (node->central_box), node->message_box, FALSE, FALSE, 0);
-        node->message_label = gtk_label_new(message);
-        gtk_widget_show(node->message_label);
-        gtk_box_pack_start (GTK_BOX (node->message_box), node->message_label, FALSE, FALSE, 0);
-    }
-    else if (node->add_info == 1) {
-        node->message_box = gtk_event_box_new();
-        gtk_widget_show(node->message_box);
-        gtk_box_pack_start (GTK_BOX (node->central_box), node->message_box, FALSE, FALSE, 0);
-        GtkWidget *label2 = gtk_label_new(message + 20);
-        gtk_widget_show(label2);
-        gtk_container_add (GTK_CONTAINER (node->message_box), label2);
-        gtk_widget_set_name(node->message_box, "file");
-        gtk_widget_add_events (node->message_box, GDK_BUTTON_PRESS_MASK);
-        g_signal_connect (G_OBJECT (node->message_box), "button_press_event", G_CALLBACK (file_callback), mes);
-        g_signal_connect (G_OBJECT (node->message_box), "button_release_event", G_CALLBACK (file1_callback), mes);
-        gtk_widget_add_events (node->message_box, GDK_ENTER_NOTIFY_MASK);
-        g_signal_connect (G_OBJECT (node->message_box), "enter_notify_event", G_CALLBACK (file_notify_callback), mes);
-        g_signal_connect (G_OBJECT (node->message_box), "leave_notify_event", G_CALLBACK (file_notify1_callback), mes);
-    }
-    else if (node->add_info == 2) {
-        node->message_box = gtk_box_new(FALSE, 0);
-        gtk_widget_show(node->message_box);
-        gtk_box_pack_start (GTK_BOX (node->central_box), node->message_box, FALSE, FALSE, 0);
-        node->image_box = gtk_box_new(FALSE, 0);
-        gtk_widget_show(node->image_box);
-        gtk_box_pack_start (GTK_BOX (node->message_box), node->image_box, FALSE, FALSE, 0);
-    }
-    else if (node->add_info == 3) {
-        GtkWidget *box2 =  gtk_box_new(FALSE, 0);
-        gtk_widget_show(box2);
-        gtk_box_pack_start (GTK_BOX (node->central_box), box2, FALSE, FALSE, 0);
-        GdkPixbuf *item_pixbuf = gdk_pixbuf_new_from_file_at_scale (message, 200, 200, TRUE, NULL);
-        GtkWidget *item_image = gtk_image_new_from_pixbuf(item_pixbuf);;
-        gtk_widget_show(item_image);
-        gtk_box_pack_start (GTK_BOX (box2), item_image, FALSE, FALSE, 0);
-    }
-    else if (node->add_info == 4) {  // for audio
-        node->message_box = gtk_event_box_new();
-        gtk_widget_show(node->message_box);
-        gtk_box_pack_start (GTK_BOX (node->central_box), node->message_box, FALSE, FALSE, 0);
-        GtkWidget *label2 = gtk_label_new(message + 20);
-        gtk_widget_show(label2);
-        gtk_container_add (GTK_CONTAINER (node->message_box), label2);
-        gtk_widget_set_name(node->message_box, "file");
-        gtk_widget_add_events (node->message_box, GDK_BUTTON_PRESS_MASK);
-        g_signal_connect (G_OBJECT (node->message_box), "button_press_event", G_CALLBACK (load_audio_callback), mes);
-    }
+    if (node->add_info == 0)
+        simple_message (node, message);
+    else if (node->add_info == 1) 
+        file (mes, node, message);
+    else if (node->add_info == 2)
+        image (node);
+    else if (node->add_info == 3)
+        sticker (node, message);
+    else if (node->add_info == 4)
+        audio (mes, node, message);
+}
+
+void create_item (GtkWidget *menu, t_mes *mes, char *title, 
+            void (*callback) (GtkWidget *widget, t_mes *mes)) {
+    GtkWidget *item = gtk_menu_item_new_with_label(title);
+
+    gtk_widget_show(item);
+    gtk_menu_shell_append (GTK_MENU_SHELL (menu), item);
+    g_signal_connect (G_OBJECT (item), "activate", G_CALLBACK (callback), mes);
 }
 
 void init_message_menu (t_message *node, t_mes *mes) {
-    node->menu_event = gtk_event_box_new();
     GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file_at_scale ("img/options.png", 20, 40, TRUE, NULL);
+    GtkWidget *menu  = gtk_menu_new ();
+
+    node->menu_event = gtk_event_box_new();
     node->menu = gtk_image_new_from_pixbuf(pixbuf);
     gtk_container_add (GTK_CONTAINER (node->menu_event), node->menu);
-    GtkWidget *menu  = gtk_menu_new ();
-    //--items
-    GtkWidget *delete = gtk_menu_item_new_with_label("Delete");
-    gtk_widget_show(delete);
-    gtk_menu_shell_append (GTK_MENU_SHELL (menu), delete);
-    g_signal_connect (G_OBJECT (delete), "activate", G_CALLBACK (delete_callback), mes);
-
-    if (node->add_info == 0) {
-        GtkWidget *edit = gtk_menu_item_new_with_label("Edit");
-        gtk_widget_show(edit);
-        gtk_menu_shell_append (GTK_MENU_SHELL (menu), edit);
-        g_signal_connect (G_OBJECT (edit), "activate", G_CALLBACK (edit_callback), mes);
-    }
-    //--
+    create_item (menu, mes, "Delete", delete_callback);
+    if (node->add_info == 0)
+        create_item (menu, mes, "Edit", edit_callback);
     gtk_widget_add_events (node->menu_event, GDK_ENTER_NOTIFY_MASK);
     gtk_widget_add_events (node->menu_event, GDK_BUTTON_PRESS_MASK);
     g_signal_connect (G_OBJECT (node->menu_event), "enter_notify_event", G_CALLBACK (focus_menu_callback), node->menu);
@@ -218,46 +235,30 @@ void init_message_menu (t_message *node, t_mes *mes) {
 }
 
 void init_message_login (t_message *node, t_mes *mes, const char *login) {
+    GtkWidget *login_menu  = gtk_menu_new ();
+    GtkWidget *view = gtk_menu_item_new_with_label("View profile");
+    GtkWidget *label = gtk_label_new(login);
+
     node->login_box = gtk_box_new(FALSE, 0);
     gtk_widget_show(node->login_box);
     node->login_event = gtk_event_box_new();
-    //--
-    GtkWidget *login_menu  = gtk_menu_new ();
-    GtkWidget *view = gtk_menu_item_new_with_label("View profile");
     gtk_widget_show(view);
     gtk_menu_shell_append (GTK_MENU_SHELL (login_menu), view);
     g_signal_connect (G_OBJECT (view), "activate", G_CALLBACK (load_profile_callback), mes);
-    //--
     gtk_widget_add_events (node->login_event, GDK_ENTER_NOTIFY_MASK);
     gtk_widget_add_events (node->login_event, GDK_BUTTON_PRESS_MASK);
     g_signal_connect (G_OBJECT (node->login_event), "enter_notify_event", 
                         G_CALLBACK (focus_callback), mes);
     g_signal_connect (G_OBJECT (node->login_event), "button_press_event", 
                         G_CALLBACK (open_menu_callback), G_OBJECT(login_menu));
-    GtkWidget *label1 = gtk_label_new(login);
-    gtk_container_add(GTK_CONTAINER(node->login_event), label1);
+    gtk_widget_set_name (label, "login");
+    gtk_container_add(GTK_CONTAINER(node->login_event), label);
     gtk_box_pack_start(GTK_BOX (node->central_box), node->login_box, FALSE, FALSE, 0);
-    gtk_widget_show(label1);
+    gtk_widget_show(label);
     gtk_widget_show(node->login_event);
 }
 
-void init_main_message (t_message *node, t_mes *mes) {
-    node->general_box = gtk_box_new(FALSE, 0);
-    gtk_widget_show(node->general_box);
-    //-events
-    node->event = gtk_event_box_new();
-    gtk_widget_show(node->event);
-    gtk_widget_add_events (node->event, GDK_ENTER_NOTIFY_MASK);
-    g_signal_connect (G_OBJECT (node->event), "enter_notify_event", G_CALLBACK (focus_callback), mes);
-    g_signal_connect (G_OBJECT (node->event), "leave_notify_event", G_CALLBACK (focus1_callback), mes);
-    gtk_box_pack_start (GTK_BOX (node->general_box), node->event, FALSE, FALSE, 0);
-    //--
-    node->main_box = gtk_box_new(FALSE, 0);
-    gtk_widget_show(node->main_box);
-    gtk_container_add (GTK_CONTAINER (node->event), node->main_box);
-    gtk_widget_set_name(node->general_box, "message");
-    gtk_container_set_border_width(GTK_CONTAINER(node->main_box), 1);
-
+void init_main_message2 (t_message *node) {
     node->left_box = gtk_box_new(FALSE, 0);
     gtk_widget_show(node->left_box);
     gtk_widget_set_size_request(node->left_box, 10, -1);
@@ -270,6 +271,23 @@ void init_main_message (t_message *node, t_mes *mes) {
     gtk_widget_show(node->right_box);
     gtk_widget_set_size_request(node->right_box, 15, -1);
     gtk_box_pack_start(GTK_BOX (node->main_box), node->right_box, FALSE, FALSE, 0);
+}
+
+void init_main_message (t_message *node, t_mes *mes) {
+    node->general_box = gtk_box_new(FALSE, 0);
+    gtk_widget_show(node->general_box);
+    node->event = gtk_event_box_new();
+    gtk_widget_show(node->event);
+    gtk_widget_add_events (node->event, GDK_ENTER_NOTIFY_MASK);
+    g_signal_connect (G_OBJECT (node->event), "enter_notify_event", G_CALLBACK (focus_callback), mes);
+    g_signal_connect (G_OBJECT (node->event), "leave_notify_event", G_CALLBACK (focus1_callback), mes);
+    gtk_box_pack_start (GTK_BOX (node->general_box), node->event, FALSE, FALSE, 0);
+    node->main_box = gtk_box_new(FALSE, 0);
+    gtk_widget_show(node->main_box);
+    gtk_container_add (GTK_CONTAINER (node->event), node->main_box);
+    gtk_widget_set_name(node->general_box, "message");
+    gtk_container_set_border_width(GTK_CONTAINER(node->main_box), 1);
+    init_main_message2(node);
 }
 
 void init_message (t_message *node, t_room *room, json_object *new_json, int order) {
@@ -315,6 +333,26 @@ void *sound_thread (void *data) {
     return 0;
 }
 
+void additional_act (t_client_info *info, t_room *room, json_object *new_json) {
+    int id = json_object_get_int(json_object_object_get(new_json, "id"));
+    int add_info = json_object_get_int(json_object_object_get(new_json, "add_info"));
+    int user_id = json_object_get_int(json_object_object_get(new_json, "user_id"));
+
+    if (info->audio == 1) {
+        if (user_id != info->id) {
+            pthread_t sound_t = NULL;
+            pthread_create(&sound_t, 0, sound_thread, NULL);
+        }
+    }
+    if (add_info == 2) {
+        t_mes *mes = (t_mes *)malloc(sizeof(t_mes));
+        mes->info = info;
+        mes->room = room;
+        mes->id = id;
+        mx_load_file(mes);
+    }
+}
+
 void mx_push_message(t_client_info *info, t_room *room, json_object *new_json) {
     t_message *tmp;
     t_message *p;
@@ -334,20 +372,5 @@ void mx_push_message(t_client_info *info, t_room *room, json_object *new_json) {
             p = p->next;
         p->next = tmp;
     }
-    int id = json_object_get_int(json_object_object_get(new_json, "id"));
-    int add_info = json_object_get_int(json_object_object_get(new_json, "add_info"));
-    int user_id = json_object_get_int(json_object_object_get(new_json, "user_id"));
-    if (info->audio == 1) {
-        if (user_id != info->id) {
-            pthread_t sound_t = NULL;
-            pthread_create(&sound_t, 0, sound_thread, NULL);
-        }
-    }
-    if (add_info == 2) {
-        t_mes *mes = (t_mes *)malloc(sizeof(t_mes));
-        mes->info = info;
-        mes->room = room;
-        mes->id = id;
-        mx_load_file(mes);
-    }
+    additional_act (info, room, new_json);
 }
